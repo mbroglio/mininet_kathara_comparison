bba4378baaaa78728c7d4b231e292303d0e33583
mbroglio <121119540+mbroglio@users.noreply.github.com>
2025-05-29 11:24:03 +0200

    Update start.sh

--- Containernet/start.sh
+++ Containernet/start.sh
@@ -1,4 +1,4 @@
 cd containernet
 sudo python3 -m venv venv
 source venv/bin/activate
-sudo -E env PATH=$PATH python3 ../Desktop/Containernet/DNS\ Simple/dns.py
+sudo -E env PATH=$PATH python3 ../Desktop/Containernet/DNS\ Kathara/containernet_dns_topo.py
